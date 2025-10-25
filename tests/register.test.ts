244a238d244f6b0ac5b39765ea939d1fb32f741d
crou <cris4ou@gmail.com>
2025-10-25 15:03:48 +0200

    test action

--- tests/register.test.ts
+++ tests/register.test.ts
@@ -9,7 +9,7 @@ it("should return 400 for invalid input", async () => {
   });
   const res = await POST(req);
 
-  expect(res.status).toBe(401);
+  expect(res.status).toBe(404);
   const json = await res.json();
   expect(json).toHaveProperty("error");
 });
