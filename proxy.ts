0d315b73f6bbf8e2807804b56028ffb394190ec3
crou <cris4ou@gmail.com>
2025-12-16 11:47:14 +0200

    migrate middleware to proxy using npx @next/codemod@canary middleware-to-proxy

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import jwt from 'jsonwebtoken';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get('auth_token')?.value;
 
   if (!token) {
