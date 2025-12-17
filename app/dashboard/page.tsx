201ecfcfd2b39a04656d40dba2ce7dd27facac76
crou <cris4ou@gmail.com>
2025-12-17 17:32:35 +0200

    login page update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,31 @@
-export default function DashboardPage() {
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { toast } from 'react-toastify';
+
+export default async function DashboardPage() {
+  const cookieStore = await cookies();  // Await the cookies
+  const token = cookieStore.get('auth_token')?.value;
+
+  if (!token) return null;
+
+  let payload: any;
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not defined');
+  }
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    redirect('/login');
+  }
+
+  // const user = await getUserById(userId);
+
+  const userId = payload.userId;
+  console.log('user ID', userId);
+  
+
   return (
     <section>
       <h2 className="text-2xl font-bold mb-4">Your Statistics</h2>
